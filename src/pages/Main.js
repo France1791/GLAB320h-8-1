fa17ce738014ab1f661df52e3d3f67e5c4f70478
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-09-16 17:09:35 -0400

    cryptoprices lab done

---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+export default function Main (props) {
+    return(
+        <>
+        <div>
+            </div> 
+        </>
+    )
+  }
---
+++ src/pages/Price.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+export default function Price (props) {
+    return (
+        <>
+        </>
+    )
+  
+  }
